a6e4edc352d89e410de153317d446126b959e25f
Frederik Darling <frederikdarling@live.dk>
2023-03-23 14:11:29 +0100

    Revert "split project backend"
    This reverts commit 09382de658cccf9bfe18c7e793bef08b8008b008.

--- src/main/java/com/example/lagaltbackend/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lagaltbackend.repository;
-
-import com.example.lagaltbackend.Model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
