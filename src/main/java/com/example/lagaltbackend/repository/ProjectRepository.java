15628d95c7455d07504843ef68e65d5ed3eeb04d
Frederik Darling <frederikdarling@live.dk>
2023-03-24 13:44:53 +0100

    security waorks now

--- src/main/java/com/example/lagaltbackend/repository/ProjectRepository.java
+++ src/main/java/com/example/lagaltbackend/repository/ProjectRepository.java
@@ -1,7 +1,6 @@
 package com.example.lagaltbackend.repository;
 
 import com.example.lagaltbackend.Model.Project;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/com/example/lagaltbackend/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.lagaltbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                // Sessions will not be used
+                .sessionManagement().disable()
+                // Disable CSRF -- not necessary when there are no sessions
+                .csrf().disable()
+                // Enable security for http requests
+                .authorizeHttpRequests(authorize -> authorize
+                   /*     .requestMatchers("/api/v1/project/list").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/goals",       "/api/v1/goals/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/workouts",    "/api/v1/workouts/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/set_counts",  "/api/v1/set_counts/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/profiles",    "/api/v1/profiles/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/user_accs",   "/api/v1/user_accs/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/addresses",   "/api/v1/addresses/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/exercises",   "/api/v1/exercises/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/activities",    "/api/v1/activities/**").permitAll()//TODO clean up*/
+
+
+                        //.requestMatchers("/api/v1/resources/authorized").hasAnyAuthority()
+                        // All endpoints are protected
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
