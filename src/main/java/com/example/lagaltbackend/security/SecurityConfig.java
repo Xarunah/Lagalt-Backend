3cf368640d225ae5bb52086837242ef0f7c356b1
Frederik Darling <frederikdarling@live.dk>
2023-03-31 22:39:17 +0200

    Deployment

--- src/main/java/com/example/lagaltbackend/security/SecurityConfig.java
+++ src/main/java/com/example/lagaltbackend/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
